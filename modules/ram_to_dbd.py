babbbeae3ab7c51aad1eec085a0ef7f427effc48
45-1-EV <43237702+45-1-EV@users.noreply.github.com>
2018-10-06 09:10:59 +0300

    10

--- modules/ram_to_dbd.py
+++ modules/ram_to_dbd.py
@@ -1,4 +1,5 @@
 import sqlite3
+from db.dbd_const import SQL_DBD_Init
 
 
 class RAMToDBD:
@@ -7,6 +8,7 @@ class RAMToDBD:
         self.connection = sqlite3.connect(db_name)
 
     def ram_to_dbd(self):
+        self.connection.cursor().executescript(SQL_DBD_Init)
         #code
         self.connection.commit()
         self.connection.close()
